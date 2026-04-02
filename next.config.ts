52914ec1f4a5dd830acf659a68f0a0d1d72c60f6
Mikilezen <mikilezen@gmail.com>
2026-04-02 09:05:27 -0700

    new

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,17 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  compress: true,
+  reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+  },
+  turbopack: {
+    root: path.join(__dirname),
+  },
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.icons8.com",
-      },
-    ],
+    domains: ["img.icons8.com", "freepik.com"],
   },
 };
 
